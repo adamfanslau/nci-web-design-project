56938013b71b8aca26b129d9e3e893d58f94c43f
lemosdaniel <daniel.alemos@hotmail.com>
2023-11-23 18:00:56 +0000

    changes to contact-about with css and js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,31 @@
 // Our javascript logic goes here:
+
+//contact us form
+document.addEventListener('DOMContentLoaded', function () {
+    const contactForm = document.getElementById('contact-form');
+    const modal = new bootstrap.Modal(document.getElementById('contactConfirmationModal'));
+
+    contactForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        modal.show();
+        contactForm.reset();
+    });
+});
+
+
+
+// subscribe newsletter form
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to show a dialog message
+    function showDialogMessage() {
+        const modal = new bootstrap.Modal(document.getElementById('subscribeConfirmationModal'));
+        modal.show();
+    }
+
+    // Attach the function to the subscribe button
+    const subscribeButton = document.getElementById('subscribeButton');
+    if (subscribeButton) {
+        subscribeButton.addEventListener('click', showDialogMessage);
+    }
+});
