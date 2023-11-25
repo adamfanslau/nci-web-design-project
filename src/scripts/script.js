d97f2d92f9f44e9ea4f11d74196349307e268219
Adam Fanslau <adam.fanslau@gmail.com>
2023-11-25 18:53:34 +0000

    added Daniels script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,16 @@
 // Our javascript logic goes here:
+
+// subscribe newsletter form
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to show a dialog message
+    function showDialogMessage() {
+        const modal = new bootstrap.Modal(document.getElementById('subscribeConfirmationModal'));
+        modal.show();
+    }
+
+    // Attach the function to the subscribe button
+    const subscribeButton = document.getElementById('subscribeButton');
+    if (subscribeButton) {
+        subscribeButton.addEventListener('click', showDialogMessage);
+    }
+});
