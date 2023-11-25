becc2fa6da7f0cfe7e2123484504f29ea53d16dd
lemosdaniel <daniel.alemos@hotmail.com>
2023-11-25 22:49:18 +0000

    script updated for contact

--- src/scripts/contact-script.js
+++ src/scripts/contact-script.js
@@ -10,3 +10,18 @@ document.addEventListener('DOMContentLoaded', function () {
         contactForm.reset();
     });
 });
+
+// subscribe newsletter form
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to show a dialog message
+    function showDialogMessage() {
+        const modal = new bootstrap.Modal(document.getElementById('subscribeConfirmationModal'));
+        modal.show();
+    }
+
+    // Attach the function to the subscribe button
+    const subscribeButton = document.getElementById('subscribeButton');
+    if (subscribeButton) {
+        subscribeButton.addEventListener('click', showDialogMessage);
+    }
+});
