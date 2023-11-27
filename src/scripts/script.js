f19aad45f6bab4c5b809c1240e1820db89a6aa6d
Anastasiia Kovalenko <nastasia.karpenko@gmail.com>
2023-11-27 05:20:26 +0000

    improved carousel(not finished). Added JS for main page. Improve responsivness for section about stages of development for main page

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,16 +1,43 @@
 // Our javascript logic goes here:
 
 // subscribe newsletter form
-document.addEventListener('DOMContentLoaded', function () {
-    // Function to show a dialog message
-    function showDialogMessage() {
-        const modal = new bootstrap.Modal(document.getElementById('subscribeConfirmationModal'));
-        modal.show();
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to show a dialog message
+  function showDialogMessage() {
+    const modal = new bootstrap.Modal(
+      document.getElementById("subscribeConfirmationModal")
+    );
+    modal.show();
+  }
 
-    // Attach the function to the subscribe button
-    const subscribeButton = document.getElementById('subscribeButton');
-    if (subscribeButton) {
-        subscribeButton.addEventListener('click', showDialogMessage);
-    }
+  // Attach the function to the subscribe button
+  const subscribeButton = document.getElementById("subscribeButton");
+  if (subscribeButton) {
+    subscribeButton.addEventListener("click", showDialogMessage);
+  }
 });
+
+function carouselImgClick(e) {
+  const target = e.target;
+  const imgDesc = target.children[0] || target;
+  const display = imgDesc.style.display;
+  console.log(display, imgDesc);
+  imgDesc.style.display = display === "block" ? "none" : "block";
+}
+
+const carousel = document.querySelector(".carousel-inner");
+carousel.addEventListener("click", carouselImgClick);
+
+function hideDescr(e) {
+  const images = document.querySelectorAll(".image-description");
+  for(let i = 0; i<images.length; i++){
+    images[i].style.display = "none";
+  
+  }
+}
+
+const next = document.querySelector(".carousel-control-next");
+const prev = document.querySelector(".carousel-control-prev");
+
+next.addEventListener("click", hideDescr);
+prev.addEventListener("click", hideDescr);
