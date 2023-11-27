731b39a7e6105d4b64458c2d99cfb18d58014e8c
Adam Fanslau <adam.fanslau@gmail.com>
2023-11-27 10:15:40 +0000

    multiple fixes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -16,28 +16,3 @@ document.addEventListener("DOMContentLoaded", function () {
     subscribeButton.addEventListener("click", showDialogMessage);
   }
 });
-
-function carouselImgClick(e) {
-  const target = e.target;
-  const imgDesc = target.children[0] || target;
-  const display = imgDesc.style.display;
-  console.log(display, imgDesc);
-  imgDesc.style.display = display === "block" ? "none" : "block";
-}
-
-const carousel = document.querySelector(".carousel-inner");
-carousel.addEventListener("click", carouselImgClick);
-
-function hideDescr(e) {
-  const images = document.querySelectorAll(".image-description");
-  for(let i = 0; i<images.length; i++){
-    images[i].style.display = "none";
-  
-  }
-}
-
-const next = document.querySelector(".carousel-control-next");
-const prev = document.querySelector(".carousel-control-prev");
-
-next.addEventListener("click", hideDescr);
-prev.addEventListener("click", hideDescr);
