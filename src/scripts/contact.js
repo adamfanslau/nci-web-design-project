e6c9458c84ee4924d5e09640d0718ef76d6cb196
lemosdaniel <daniel.alemos@hotmail.com>
2023-11-28 13:25:47 +0000

    Added list of services to About Us Added email validation to Subscribe to our newsletter Added image description to About us Added comment to all code for about.html/contact.html/contact.js/script.js Added comment to footer for all HTML pages Added Author Meta tag for contct and about.html

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,4 +1,6 @@
-//contact us form
+//author: Daniel Lemos
+
+//contact us form script
 document.addEventListener('DOMContentLoaded', function () {
     const contactForm = document.getElementById('contact-form');
     const modal = new bootstrap.Modal(document.getElementById('contactConfirmationModal'));
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,27 @@
-// Our javascript logic goes here:
-
 // subscribe newsletter form
 document.addEventListener("DOMContentLoaded", function () {
+  // Function to validate email format
+  function isValidEmail(email) {
+    // Regular expression for a basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
   // Function to show a dialog message
   function showDialogMessage() {
-    const modal = new bootstrap.Modal(
-      document.getElementById("subscribeConfirmationModal")
-    );
-    modal.show();
+    const emailInput = document.getElementById("newsletter1");
+    const email = emailInput.value.trim();
+
+    if (isValidEmail(email)) {
+      // Valid email, show the confirmation modal
+      const modal = new bootstrap.Modal(
+        document.getElementById("subscribeConfirmationModal")
+      );
+      modal.show();
+    } else {
+      // Invalid email, show an error message 
+      alert("Please enter a valid email address.");
+    }
   }
 
   // Attach the function to the subscribe button
